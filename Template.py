f92fb8465acc497e73b283df7d40e11a782c074d
Claudia Correia <claudia.f.correia@hotmail.com>
2017-04-27 17:24:12 +0100

    add Jinja2 Template

---
+++ Template.py
@@ -0,0 +1,24 @@
+import struct
+from copy import copy
+
+{% for mabbrev in mabbrev_list %}
+class {{mabbrev}}(Message):
+    {% for abbrev in abbrev_list -%}
+    def get_{{abbrev}}(self):
+        print "{{abbrev}} - " + str(self.{{abbrev}})
+        
+    {% endfor %}
+
+    {% for abbrev in abbrev_list -%}
+    def set_{{abbrev}}(self,new):
+        self.{{abbrev}} = new
+        
+    {% endfor %}
+    def get_name(self):
+        return "{{mabbrev}}"
+
+    def clone(self):
+        return copy(self)
+
+    
+{% endfor %}
