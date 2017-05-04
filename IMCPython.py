c6d81e3023632662b88e89777b986a1cab41d282
Claudia Correia <claudia.f.correia@hotmail.com>
2017-05-04 15:40:07 +0100

    create tuple with abbrev and type

--- IMCPython.py
+++ IMCPython.py
@@ -9,6 +9,71 @@ class IMCPython:
             self.tree = ElementTree.parse(f)
         self.abbrev_lista = []
         self.mabbrev_lista = []
+        self.type_lista = []
+        self.abb_type = []
+        self.THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+        self.j2_env = Environment(loader=FileSystemLoader(self.THIS_DIR), trim_blocks = True)
+        self.out = ""
+            
+    def parse_message(self):
+        for node in self.tree.iter('message'):
+            m_id = node.attrib.get('id')
+            m_name = node.attrib.get('name')
+            m_abbrev = node.attrib.get('abbrev')
+            m_source = node.attrib.get('source')
+            self.parse_field(node)
+            if m_abbrev is not None:
+                self.mabbrev_lista.append(m_abbrev)
+           
+            aux = self.j2_env.get_template('Template.py').render(abb_type_list = self.abb_type, mabbrev_list = self.mabbrev_lista)
+            self.out += aux
+            self.out += "\n"
+            self.abbrev_lista = []
+            self.mabbrev_lista = []  
+            #self.abb_type = []          
+           
+           #criar tuplo com o abbrev e o tipo 
+                               
+    def parse_field(self, message_node):
+        for field_node in message_node.iter('field'):
+            name = field_node.attrib.get('name')
+            abbrev = field_node.attrib.get('abbrev')
+            type = field_node.attrib.get('type')
+            unit = field_node.attrib.get('unit')
+            
+            if abbrev is not None:
+                self.abbrev_lista.append(abbrev)
+                
+            if type is not None:
+            	self.type_lista.append(type)
+            	
+       	self.abb_type = zip(self.abbrev_lista,self.type_lista)
+
+nw = IMCPython()
+nw.parse_message()
+
+with open('src_generated/Definitions.py','wb') as f:
+    f.write('import struct\n')
+with open("src_generated/Definitions.py", "ab") as f:
+    f.write("from copy import deepcopy\n")
+with open("src_generated/Definitions.py", "ab") as f:
+    f.write(nw.out.encode('latin-1'))
+
+    #copiar o message.py para o src_generated
+with open('Message.py','rb') as ms:
+    data = ms.read()
+    with open('src_generated/Message.py','wb') as m:
+        m.write(data)#!/usr/bin/env/python
+from jinja2 import Environment, FileSystemLoader
+import os
+from xml.etree import ElementTree
+
+class IMCPython:    
+    def __init__(self):
+        with open('imc.xml', 'rt') as f:
+            self.tree = ElementTree.parse(f)
+        self.abbrev_lista = []
+        self.mabbrev_lista = []
         self.THIS_DIR = os.path.dirname(os.path.abspath(__file__))
         self.j2_env = Environment(loader=FileSystemLoader(self.THIS_DIR), trim_blocks = True)
         self.out = ""
--- Template.py
+++ Template.py
@@ -1,4 +1,33 @@
+{% for mabbrev in mabbrev_list %}
+class {{mabbrev}}(Message):
+  
+    {% for abbrev, Type in abb_type_list if ((Type == "uint8_t" ) or (Type == "int8_t") or (Type == 'int16_t') or (Type == 'uint16_t') or (Type == 'int32_t') or (Type == 'uint32_t') or (Type == 'int64_t') or (Type == 'fp32_t') or (Type == 'fp64_t')) -%}
+    self.{{abbrev}} = 0
+    {% endfor -%}
+    	
+   	{%- for abbrev, Type in abb_type_list if ((Type != "uint8_t" ) and (Type != "int8_t") and (Type != 'int16_t') and (Type != 'uint16_t') and (Type != 'int32_t') and (Type != 'uint32_t') and (Type != 'int64_t') and (Type != 'fp32_t') and (Type != 'fp64_t')) -%}
+	self.{{abbrev}} = ""
+    {% endfor %}	
+    {% for abbrev, Type in abb_type_list -%}
+    def get_{{abbrev}}(self):
+        return self.{{abbrev}}
 
+    {% endfor -%}
+
+    {% for abbrev, Type in abb_type_list -%}
+    def set_{{abbrev}}(self,new):
+        self.{{abbrev}} = new
+        
+    {% endfor -%}
+    def get_abbrev(self):
+        return "{{mabbrev}}"
+
+    def clone(self):
+        return deepcopy(self) 
+        
+    def serialize(self):
+     	#return 
+{% endfor %}
 {% for mabbrev in mabbrev_list %}
 class {{mabbrev}}(Message):
     {% for abbrev in abbrev_list -%}
