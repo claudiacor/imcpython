f68b0b7a3f2781dc66baaff77f95df7476567a7d
Claudia Correia <claudia.f.correia@hotmail.com>
2017-05-04 15:45:07 +0100

    remove duplicate

--- Template.py
+++ Template.py
@@ -28,24 +28,3 @@ class {{mabbrev}}(Message):
     def serialize(self):
      	#return 
 {% endfor %}
-{% for mabbrev in mabbrev_list %}
-class {{mabbrev}}(Message):
-    {% for abbrev in abbrev_list -%}
-    def get_{{abbrev}}(self):
-        print "{{abbrev}} - " + str(self.{{abbrev}})
-        
-    {% endfor %}
-
-    {% for abbrev in abbrev_list -%}
-    def set_{{abbrev}}(self,new):
-        self.{{abbrev}} = new
-        
-    {% endfor -%}
-    def get_name(self):
-        return "{{mabbrev}}"
-
-    def clone(self):
-        return copy(self)
-
-    
-{% endfor %}
