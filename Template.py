fec8c4eb36bef21d4b717ded679dd4a4f6375f69
Claudia Correia <claudia.f.correia@hotmail.com>
2017-05-16 18:44:38 +0100

    tuple with format, and create serielizeFields()

--- Template.py
+++ Template.py
@@ -1,20 +1,25 @@
 {% for mabbrev in mabbrev_list %}
 class {{mabbrev}}(Message):
   
-    {% for abbrev, Type in abb_type_list if ((Type == "uint8_t" ) or (Type == "int8_t") or (Type == 'int16_t') or (Type == 'uint16_t') or (Type == 'int32_t') or (Type == 'uint32_t') or (Type == 'int64_t') or (Type == 'fp32_t') or (Type == 'fp64_t')) -%}
+    {% for abbrev, Type, Format in abb_type_form_list -%}
+    {%-if ((Type == "uint8_t" ) or (Type == "int8_t") or (Type == 'int16_t') or (Type == 'uint16_t') or (Type == 'int32_t') or (Type == 'uint32_t') or (Type == 'int64_t') or (Type == 'fp32_t') or (Type == 'fp64_t')) -%}
     self.{{abbrev}} = 0
-    {% endfor -%}
+    {% elif Type == 'plaintext' -%}
+    self.{{abbrev}} = ""
+    {% elif (Type == 'rawdata' or Type == 'message-list') -%}
+    self.{{abbrev}} = []
+    {% elif (Type == 'message') -%}
+    self.{{abbrev}} = None
+    {% endif -%}
+    {% endfor %}
     	
-   	{%- for abbrev, Type in abb_type_list if ((Type != "uint8_t" ) and (Type != "int8_t") and (Type != 'int16_t') and (Type != 'uint16_t') and (Type != 'int32_t') and (Type != 'uint32_t') and (Type != 'int64_t') and (Type != 'fp32_t') and (Type != 'fp64_t')) -%}
-	self.{{abbrev}} = ""
-    {% endfor %}	
-    {% for abbrev, Type in abb_type_list -%}
+    {% for abbrev, Type, Format in abb_type_form_list -%}
     def get_{{abbrev}}(self):
         return self.{{abbrev}}
 
     {% endfor -%}
 
-    {% for abbrev, Type in abb_type_list -%}
+    {% for abbrev, Type, Format in abb_type_form_list -%}
     def set_{{abbrev}}(self,new):
         self.{{abbrev}} = new
         
@@ -27,4 +32,13 @@ class {{mabbrev}}(Message):
         
     def serialize(self):
      	#return 
+
+	{% if Type is not none %}
+	def serializeFields(self, buffer, offset = DUNE_IMC_CONST_HEADER_SIZE):
+   		{% for abbrev, Type, Format in abb_type_form_list -%}    
+	struct.pack_into('<' + {{Format}},buffer,offset, self.{{abbrev}})
+   		{% endfor %}
+  	{% endif -%}
 {% endfor %}
+
+
