d309ce56ce4eb059f906f756537dc5f6af7055e7
Claudia Correia <claudia.f.correia@hotmail.com>
2017-05-26 00:06:21 +0100

    correct indentation and abb_type_form_list|length != 0

--- Template.py
+++ Template.py
@@ -1,14 +1,17 @@
 {% for mabbrev in mabbrev_list %}
 class {{mabbrev}}(Message):
-  
+   	
+{% if abb_type_form_list|length != 0 %}
+    def __init__(self):
+{% endif %}
     {% for abbrev, Type, Format in abb_type_form_list -%}
-    {%-if ((Type == "uint8_t" ) or (Type == "int8_t") or (Type == 'int16_t') or (Type == 'uint16_t') or (Type == 'int32_t') or (Type == 'uint32_t') or (Type == 'int64_t') or (Type == 'fp32_t') or (Type == 'fp64_t')) -%}
+    {% if ((Type == "uint8_t" ) or (Type == "int8_t") or (Type == 'int16_t') or (Type == 'uint16_t') or (Type == 'int32_t') or (Type == 'uint32_t') or (Type == 'int64_t') or (Type == 'fp32_t') or (Type == 'fp64_t')) %}
     self.{{abbrev}} = 0
-    {% elif Type == 'plaintext' -%}
+    {% elif Type == 'plaintext' %}
     self.{{abbrev}} = ""
-    {% elif (Type == 'rawdata' or Type == 'message-list') -%}
+    {% elif (Type == 'rawdata' or Type == 'message-list') %}
     self.{{abbrev}} = []
-    {% elif (Type == 'message') -%}
+    {% elif (Type == 'message') %}
     self.{{abbrev}} = None
     {% endif -%}
     {% endfor %}
@@ -30,15 +33,22 @@ class {{mabbrev}}(Message):
     def clone(self):
         return deepcopy(self) 
         
-    def serialize(self):
+    #def serialize(self):
      	#return 
-
-    {% if Type is not none -%}
-	def serializeFields(self, buffer, offset = DUNE_IMC_CONST_HEADER_SIZE):
-   		{% for abbrev, Type, Format in abb_type_form_list -%}    
+     	
+{% if abb_type_form_list|length != 0 %}
+    def serializeFields(self, buffer, offset = DUNE_IMC_CONST_HEADER_SIZE):
+    {% for abbrev, Type, Format in abb_type_form_list if Type is not none %}
 	struct.pack_into('<' + {{Format}},buffer,offset, self.{{abbrev}})
-   		{% endfor -%}
-    {% endif -%}
+        offset += struct.calcsize('<' + {{Format}})
+    {% endfor %}
+
+    def deserializeFields(self, buffer, offset = DUNE_IMC_CONST_HEADER_SIZE):
+    {% for abbrev, Type, Format in abb_type_form_list if Type is not none %}
+    self.{{abbrev}} = struct.unpack_from('<' + {{Format}},buffer,offset)
+        offset += struct.calcsize('<' + {{Format}})
+    {% endfor %}
+{% endif %}
 {% endfor %}
 
 
