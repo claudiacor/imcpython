35e046091ad80fbe0b3057d999fc265246bff5ed
Claudia Correia <claudia.f.correia@hotmail.com>
2017-05-30 15:13:02 +0100

    correct size in serialization and deserialization

--- Template.py
+++ Template.py
@@ -38,15 +38,28 @@ class {{mabbrev}}(Message):
      	
 {% if abb_type_form_list|length != 0 %}
     def serializeFields(self, buffer, offset = DUNE_IMC_CONST_HEADER_SIZE):
-    {% for abbrev, Type, Format in abb_type_form_list if Type is not none %}
+    {% for abbrev, Type, Format in abb_type_form_list if Type is not none -%}
+   	{% if Type == 'rawdata' or Type == 'plaintext' %}
+    nbytes = len(self.{{abbrev}})
+	struct.pack_into('<H' + str(nbytes) + {{Format}},buffer,offset, nbytes, self.{{abbrev}})
+        offset += struct.calcsize('<' + str(nbytes) + {{Format}})
+	{% else %}
 	struct.pack_into('<' + {{Format}},buffer,offset, self.{{abbrev}})
-        offset += struct.calcsize('<' + {{Format}})
+    	offset += struct.calcsize('<' + {{Format}})
+    {% endif %}  
     {% endfor %}
 
     def deserializeFields(self, buffer, offset = DUNE_IMC_CONST_HEADER_SIZE):
-    {% for abbrev, Type, Format in abb_type_form_list if Type is not none %}
+    {% for abbrev, Type, Format in abb_type_form_list if Type is not none -%}
+    {% if Type == 'rawdata' or Type == 'plaintext' %}
+    Nbytes = struct.unpack_from('<H',buffer,offset)
+    	offset += struct.calcsize('<H')
+    	self.{{abbrev}} = struct.unpack_from('<' + str(Nbytes) + {{Format}},buffer,offset)
+        offset += struct.calcsize('<' + str(Nbytes) + {{Format}})
+    {% else %}
     self.{{abbrev}} = struct.unpack_from('<' + {{Format}},buffer,offset)
         offset += struct.calcsize('<' + {{Format}})
+    {% endif %} 
     {% endfor %}
 {% endif %}
 {% endfor %}
